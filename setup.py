95f0fca7c660dacf52d6ac1d94adac8dd43fe526
samwiseog <samtrojan96@gmail.com>
2025-10-21 22:09:59 -0400

    fix funtion to enhance query

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ def main():
     query_rag(query_text)
 
 def enhance_query(query):
-    prompt = PROMPT_TEMPLATE.format( query=query)
+    prompt = query_enhancement_prompt.format( query=query)
     # print(prompt)
 
     init_message = [{'role': 'user', 'content': prompt}]
