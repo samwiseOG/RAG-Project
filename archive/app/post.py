eb013a6e7fd7ff36019e75bf394406cc74ba28b5
samwiseog <samtrojan96@gmail.com>
2026-07-27 19:05:30 -0400

    update

--- archive/app/post.py
+++ archive/app/post.py
@@ -4,5 +4,5 @@ file_path = input("file to embed: ")
 
 with open(file_path, 'r') as f:
     files = {'document': f}
-    response = requests.post("http://api.baiyi-sam.site/embed")
+    response = requests.post("http://api.baiyi-sam.site/embed",files)
 print(response.text)
